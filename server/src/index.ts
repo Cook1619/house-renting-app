1a718943200bfdcabeed4fca6365df6e598b8576
Cook1619 <cook6348@gmail.com>
2020-04-11 16:32:35 -0500

    Fetching data from mongo atlas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,24 @@
-import express from "express";
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
-import { typeDefs, resolvers } from './graphql'
-const app = express();
+import { connectDatabase } from "./database";
+import { typeDefs, resolvers } from "./graphql";
 const PORT = 9000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: "/api" });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
+  server.applyMiddleware({ app, path: "/api" });
 
-app.listen(PORT);
+  app.listen(PORT);
 
-console.log(`[app]: http://localhost:${PORT}`);
+  console.log(`[app]: http://localhost:${PORT}`);
+
+  const listings = db.listings.find({}).toArray();
+  console.log(listings);
+};
+
+mount(express());
