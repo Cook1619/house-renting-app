12d8b030ee0bf9c81a7f59153b2d6e4be02c2e5e
Cook1619 <cook6348@gmail.com>
2020-04-10 20:33:58 -0500

    Adding eslint rules, setting up some dummy data to work with, and tetsing new routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,24 @@
-import express from 'express'
+import express from "express";
+import bodyParser from "body-parser";
+import { listings } from "./listings";
 const app = express();
-const PORT = 9000
+const PORT = 9000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Testing')
+app.use(bodyParser.json());
+//To not get an error about not using an arguement we use an underscore before arguement name
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send("failed to delete listing");
 });
 
 app.listen(PORT);
