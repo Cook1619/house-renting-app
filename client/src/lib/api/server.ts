759aa36b51fe5150d5c48aaae8ed1a721bf04783
Cook1619 <cook6348@gmail.com>
2020-04-16 21:39:39 -0500

    Type checking out request data coming in to listings component

--- client/src/lib/api/server.ts
+++ client/src/lib/api/server.ts
@@ -1,18 +1,16 @@
 interface Body {
-    query: string;
+  query: string;
 }
 
-
 export const server = {
-    fetch: async (body: Body) => {
-        const res = await fetch('/api', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(body),
-        });
-        return res.json();
-    }
+  fetch: async <TData = any>(body: Body) => {
+    const res = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    return res.json() as Promise<{ data: TData }>;
+  },
 };
-
